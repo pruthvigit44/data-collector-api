1a4c15ca898da10e95741a684f13926b5cd0a3f6
pruthvigit44 <pruthvigit44@gmail.com>
2025-06-21 01:13:21 +0530

    Cron job

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors';
+import job from './lib/cron.js';
 import "dotenv/config";
 import authRoutes from './routes/authRoutes.js';
 import { connectDB } from './lib/db.js';
@@ -8,7 +10,9 @@ import data from './routes/data.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+job.start(); // Start the cron job
 app.use(express.json()); // Middleware to parse JSON bodies
+app.use(cors()); // Enable CORS for all routes
 // console.log( { PORT });
 
 app.use("/api/auth",authRoutes);
