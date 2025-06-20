0909656b70ce61904db3c7849ef33f086c484f92
pruthvigit44 <pruthvigit44@gmail.com>
2025-06-21 00:29:41 +0530

    user model updated

--- src/models/User.js
+++ src/models/User.js
@@ -14,11 +14,11 @@ const userSchema = new mongoose.Schema({
     unique: true,
     trim: true,
   },
-  class : {
-    type: String,
-    required: true,
-    trim: true,
-  },
+  // class : {
+  //   type: String,
+  //   required: true,
+  //   trim: true,
+  // },
   password: {
     type: String,
     required: true,
